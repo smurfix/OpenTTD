d5d2bc1d8b34e35ce985ef1af5d69b26698d1054
Matthias Urlichs <matthias@urlichs.de>
2025-01-21 10:44:50 +0100

    Add: Python support: "_ttd.debug" writes to the OpenTTD debug output
    Does not use locking or message passing.

--- src/python/openttd/_main.py
+++ src/python/openttd/_main.py
@@ -206,7 +206,7 @@ class Main:
             n = 1
             while True:
                 await anyio.sleep(n)
-                log("Python running: %d",n)
+                _ttd.debug(3, f"Python running: {n}")
                 n *= 2
 
 def run():
