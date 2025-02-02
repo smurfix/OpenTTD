62c350ece3d0d767df7d60fb2ffed4da90a0dd96
Matthias Urlichs <matthias@urlichs.de>
2025-02-02 15:46:56 +0100

    Python: Add the extension_of class mangler.
    This is used to patch Python classes transparently into C++-derived
    ones (mainly enums).
    
    This is necessary because Python's "enum" blocks subclassing.

--- src/python/openttd/util.py
+++ src/python/openttd/util.py
@@ -7,6 +7,74 @@
 """
 Some utility functions
 """
+from __future__ import annotations
+
+class _add_new_checker(type):
+    def __instancecheck__(cls,obj):
+        return type(obj) in (cls,cls._DERIV__Base)
+    def __call__(cls,*a,**kw):
+        return cls._DERIV__Base(*a,**kw)
+    def __repr__(self):
+        return repr(self._DERIV__Base)
+    def __str__(self):
+        return str(self._DERIV__Base)
+    def __getattr__(self, k):
+        return self._DERIV__Base.__getattr__(k)
+
+def extension_of(base):
+    """
+    Mostly-transparently extend a class or enum with additional methods.
+    This is a shorthand for monkeypatching the base class.
+
+    Usage::
+
+        class Base(enums.Enum):
+            A=1
+            B=2
+            C=3
+
+        @for_enum(Base)
+        class Deriv:
+            def hello(self,x):
+                assert isinstance(self,Base)
+                return x+self.value
+
+        x1 = Deriv(2)
+        x2 = Deriv(x1)
+        x3 = Base(2)
+        assert x1 is x2
+        assert x1 is x3
+        assert x3.hello(42) == 44
+    """
+    def mangler(deriv):
+        import _ttd
+        import os
+        if "PY_OTTD_STUB_GEN" in os.environ:
+            return deriv
+        for k in dir(deriv):
+            if k.startswith("__") and hasattr(base,k):
+                continue
+            v = getattr(deriv,k)
+            setattr(base,k,v)
+        for k in ("__members__",):
+            try:
+                v = getattr(base,k)
+            except AttributeError:
+                pass
+            else:
+                try:
+                    setattr(deriv,k,v)
+                except AttributeError:
+                    pass
+#                   for kk,vv in v.items():
+#                       deriv.__members__[k] = v
+
+
+        class DERIV(deriv,metaclass=_add_new_checker):
+            __Base=base
+        DERIV.__name__=deriv.__name__
+        return DERIV
+    return mangler
 
 async def maybe_async(fn, *a, **kw):
     res = fn(*a, **kw)
