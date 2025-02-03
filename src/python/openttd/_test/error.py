32fc5458e62c6aa7edd3f88fd8bd80852261d5ab
Matthias Urlichs <matthias@urlichs.de>
2025-02-03 15:45:57 +0100

    Python: move non-"all" tests do their own scripts

---
+++ src/python/openttd/_test/error.py
@@ -0,0 +1,24 @@
+#
+# This file is part of OpenTTD.
+# OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+# OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Error test. Crashes.
+"""
+
+from __future__ import annotations
+
+import anyio
+import openttd
+from openttd.base import BaseScript
+from openttd._main import VEvent
+from . import TestScript
+from .._ import TTDError
+
+class Script(TestScript):
+
+    def test(self):
+        raise RuntimeError("Exiting with an error.")
+
